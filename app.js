8bf9d79728efe69385386557a4fa6bc3cbee3bd0
Richona <105828822+Richona@users.noreply.github.com>
2022-07-05 15:24:17 -0300

    add register

--- app.js
+++ app.js
@@ -6,5 +6,6 @@ const port = 3030;
 app.use(express.static("public"));
 
 app.get("/", (req,res) => res.sendFile(path.resolve(__dirname, "views/home.html")));
+app.get("/registro", (req,res) => res.sendFile(path.resolve(__dirname, "views/registro.html")));
 
 app.listen(port, () => console.log("Server is run in http://localhost:" +port));
